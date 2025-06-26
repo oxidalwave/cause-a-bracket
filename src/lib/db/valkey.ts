e4e73cc588c84f9961b0be1a271f9bb61829716c
OxidalWave <github@oxidalwave.com>
2025-06-26 15:17:29 -0500

    Use VITE_VALKEY_CONNECTION_STRING

--- src/lib/db/valkey.ts
+++ src/lib/db/valkey.ts
@@ -1,12 +1,6 @@
 import Valkey from "iovalkey";
 import env from "../env";
 
-const valkey = new Valkey({
-	host: env.VITE_VALKEY_HOST,
-	port: env.VITE_VALKEY_PORT,
-	username: env.VITE_VALKEY_USERNAME,
-	password: env.VITE_VALKEY_PASSWORD,
-	db: env.VITE_VALKEY_DB,
-});
+const valkey = new Valkey(env.VITE_VALKEY_CONNECTION_STRING);
 
 export default valkey;
--- src/lib/env/index.ts
+++ src/lib/env/index.ts
@@ -1,11 +1,7 @@
 import z from "zod";
 
 const EnvironmentValidator = z.object({
-	VITE_VALKEY_HOST: z.string(),
-	VITE_VALKEY_PORT: z.coerce.number().int().min(0).max(65535).optional(),
-	VITE_VALKEY_USERNAME: z.string().optional(),
-	VITE_VALKEY_PASSWORD: z.string().optional(),
-	VITE_VALKEY_DB: z.coerce.number().int().min(0).optional(),
+	VITE_VALKEY_CONNECTION_STRING: z.string().url(),
 });
 
 const env = EnvironmentValidator.parse(import.meta.env);
