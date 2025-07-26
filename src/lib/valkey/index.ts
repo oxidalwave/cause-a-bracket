d982925ebf7cfef44c0a3b62a98392f7b09d6dfb
OxidalWave <github@oxidalwave.com>
2025-07-26 15:18:34 -0500

    Update mise, use VITE_ prefix for valkey

--- src/lib/valkey/index.ts
+++ src/lib/valkey/index.ts
@@ -1,5 +1,5 @@
 import Valkey from "iovalkey";
 
-const valkey = new Valkey(import.meta.env.VALKEY_CONNECTION_STRING);
+const valkey = new Valkey(import.meta.env.VITE_VALKEY_CONNECTION_STRING);
 
 export default valkey;
