32809ae8454f552b9ca5b2fc35d6210c5ed145be
OxidalWave <github@oxidalwave.com>
2025-08-02 21:51:46 -0500

    Set up storybook; dark mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,46 @@
+/// <reference types="vitest/config" />
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-	server: {
-		port: 3000,
-	},
-	plugins: [
-		tailwindcss(),
-		tsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
-		tanstackStart({
-			target: "node-server",
-			customViteReactPlugin: true
-		}),
-		react(),
-	],
+  server: {
+    port: 3000
+  },
+  plugins: [tailwindcss(), tsConfigPaths({
+    projects: ["./tsconfig.json"]
+  }), tanstackStart({
+    target: "node-server",
+    customViteReactPlugin: true
+  }), react()],
+  test: {
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: 'playwright',
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
+  }
 });
