690d93b1aaaf9a203fd61459fb26099b6fd26e49
OxidalWave <github@oxidalwave.com>
2025-08-09 17:02:27 -0500

    Fix build

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
-import { Card, Flex } from "@mantine/core";
+import { Stack } from "@mantine/core";
 import { createFileRoute } from "@tanstack/react-router";
-import Twist from "~/components/animation/Twist";
 import TsLink from "~/components/TsLink";
 
 export const Route = createFileRoute("/")({
@@ -9,22 +8,8 @@ export const Route = createFileRoute("/")({
 
 function Home() {
   return (
-    <Flex align="center" justify="center" h="100%">
-      <Twist>
-        <Card>
-          <Card.Section>
-            <TsLink.Button
-              to="/brackets/$id"
-              params={{
-                id: "abcd",
-              }}
-              size="xl"
-            >
-              Cause a Bracket!
-            </TsLink.Button>
-          </Card.Section>
-        </Card>
-      </Twist>
-    </Flex>
+    <Stack>
+      <TsLink.Button to="/categories">View Categories</TsLink.Button>
+    </Stack>
   );
 }
